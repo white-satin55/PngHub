bf11c108bdc7c472012ee6c480a9755115aa280a
Egor Sazanov <egorsazanov97@gmail.com>
2023-09-08 00:39:08 +0300

    Project initialized

---
+++ PngHub.Domain/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace PngHub.Domain;
+public class Image
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public byte[] Content { get; set; }
+}
